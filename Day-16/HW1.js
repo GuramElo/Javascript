fe23689433ca5dcc374f6d99a775160ae585c98f
guri <g_elizbarashvili@cu.edu.ge>
2021-09-03 00:05:07 -0700

    All of it

---
+++ Day-16/HW1.js
@@ -0,0 +1,3 @@
+var a=[2, 5, 9, 15, 0, 4],b,c="";
+for(b=0;b<a.length;b++)if(a[b]>3 && a[b]<10)c=c+a[b]+" ";
+console.log(c);
---
+++ Day-16/HW2.js
@@ -0,0 +1,5 @@
+var n=1000,num=0;
+while(n>50){
+    n/=2;
+    num++;}
+console.log(n+'  ',num);
